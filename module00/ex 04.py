ae8ce4cd01afac27ebe9f46a0a3c9bbeffaed944
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 00:05:46 +0200

    Create ex 04.py

---
+++ module00/ex 04.py
@@ -0,0 +1,11 @@
+def operation(nb1,nb2):
+    S=nb1+nb2
+    D=nb1-nb2
+    P=nb1*nb2
+    Q=nb1/nb2
+    R=nb1%nb2
+    print("Sum=",S)
+    print("Difference=",D)
+    print("Product=",P )   
+    print("Quotient=",Q)
+    print("Remainder=",R)
