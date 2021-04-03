61bc02983a9afb7d30da4ef5eee131e8d2c8ce7f
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 14:07:24 +0200

    Update ex 09.py

--- module00/ex 09.py
+++ module00/ex 09.py
@@ -2,7 +2,7 @@ import random
 n=random.randint(1,99)
 print("This is an interactive guessing game!You have to enter a number between 1 and 99 to find out the secret number.Type 'exit' to end the game.Good luck!")
 m=int(input("What's your guess between 1 and 99?"))
-coup=0
+coup=1
 while (m != n):
     if (n> m):
         print("Too low!");
