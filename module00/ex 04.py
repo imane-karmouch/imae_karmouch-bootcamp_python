11dbc66dec276e1471bbdbf7c21164c2a46088e3
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 16:22:42 +0200

    Update ex 04.py

--- module00/ex 04.py
+++ module00/ex 04.py
@@ -2,8 +2,14 @@ def operation(nb1,nb2):
     S=nb1+nb2
     D=nb1-nb2
     P=nb1*nb2
-    Q=nb1/nb2
-    R=nb1%nb2
+    if type(nb1)==str or type(nb2)==str:
+        print("ERROR")
+    if nb2!=0:
+        Q=nb1/nb2
+        R=nb1%nb2
+    else:
+        Q="ERROR"
+        R="ERROR"
     print("Sum=",S)
     print("Difference=",D)
     print("Product=",P )   
