00966d2f88122cec4c01d252f91f35016d209551
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 21:32:47 +0200

    Update ex01.py

--- module01/ex01.py
+++ module01/ex01.py
@@ -4,5 +4,26 @@ class GotCharacter:
   def __init_(self,n,alive):
     self.first_name=n
     self.is_alive=alive
+class Stark(GotCharacter):
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Stark"
+        self.house_words = "Winter is Coming"
+    def print_house_words(self):
+      print(self.house_words)
+    def die(self):
+      self.is_alive=False
     
+    
+    #test
+from game import GotCharacter, Stark
+arya = Stark("Arya")
+print(arya.__dict__)
+
+arya.print_house_words()
+
+print(arya.is_alive)
+arya.die()
+print(arya.is_alive)
+
     
