b5487921a0480931b9a898cd27f8e1a448e0e1ec
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 11:40:43 +0200

    Create kata01.py

---
+++ module00/ex 05.py/kata01.py
@@ -0,0 +1,7 @@
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+for cle, valeur in languages.items():
+    print(cle,"was created by", valeur)
