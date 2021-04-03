2e497382a4cb3e6425d4ba2fc8975d2c9b46c2f3
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 18:49:33 +0200

    Update book.py

--- module01/ex00/book.py
+++ module01/ex00/book.py
@@ -15,3 +15,17 @@ class book:
       txt=''
       txt+=self.name+','+str(self.last_update)+','+str(self.creation_date)+','+self.recipes_list
       return txt
+    
+    
+    
+  def get_recipe_by_name(self, name):
+    """Print a recipe with the name `name` and return the instance"""
+    pass
+
+  def get_recipes_by_types(self, recipe_type):
+    """Get all recipe names for a given recipe_type """
+    pass
+
+  def add_recipe(self, recipe):
+    """Add a recipe to the book and update last_update"""
+    pass
