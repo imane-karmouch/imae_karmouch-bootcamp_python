de336979dec01e79a14b370dfb14241c2a03139e
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2021-04-03 13:31:04 +0200

    Update ex 09.py

--- module00/ex 09.py
+++ module00/ex 09.py
@@ -1,1 +1,13 @@
-
+import random 
+n=random.randint(1,99)
+m=int(input("donner un nombre"))
+coup=0
+while (m != n):
+    if (n> m):
+        print("Too low!");
+    else:
+        if (n < m):
+            print("Too high!");
+    m=int(input("donner un nombre"))
+    coup+=1
+print ("Congratulations, you've got it in :",coup,"tries");
